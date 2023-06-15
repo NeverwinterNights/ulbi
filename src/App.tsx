4e2f078fb980db4fd6e2e0aaa8b621759b2530b1
pavelcardash <cardash@mail.ru>
2023-06-15 23:49:04 +0300

    les 5 done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {Counter} from "./components/Counter";
 import "./index.scss"
 
 export const App =React.memo(() => {
+    console.log ("value",)
  return (
         <div className="app">
             <Counter/>
